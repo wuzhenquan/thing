39f06cf97bee9b2370bab13e8267d050c7bf9247
巫镇泉 <wuzhenquan1990@gmail.com>
2019-01-20 17:37:00 +0800

    **fontend** add basic todo

--- frontend/my-app/src/components/workspaces/todo/Todo.jsx
+++ frontend/my-app/src/components/workspaces/todo/Todo.jsx
@@ -1,24 +1,82 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import Icon from '../../icons/Icon'
+import TodoItem from './TodoItem'
 
-function Todo() {
-    return (
-        <div>
+class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            todoArr: [],
+            focusTodoId: 0
+        }
+        this.textareaRef = React.createRef();
+    }
+
+    addTodo() {
+        const todoArr = this.state.todoArr
+        const order = todoArr.length + 1
+        const id = order
+        const content = ''
+        this.changeFocusId(id)
+        this.setState((state) => {
+            const todoArr = [{ id, content, order }].concat(state.todoArr)
+            return { todoArr }
+        }, () => {
+            // next add request todo api
+            // only request successful, then we can add next
+        })
+    }
+
+    editTodo = (info, index) => {
+        this.setState((state) => {
+            let todoArr = state.todoArr
+            todoArr[index] = info
+            return { todoArr }
+        })
+        this.changeFocusId(0)
+    }
+
+    changeFocusId = (todoId) => {
+        this.setState({ focusTodoId: todoId || 0 })
+    }
+
+    render() {
+        return (
             <div>
-                <span className="pointer" onClick={ () => { } }>
-                    <Icon name='plus' />
-                    Add todo
+                <div>
+                    <span className="pointer" onClick={ () => { this.addTodo() } }>
+                        <Icon name='plus' />
+                        Add todo
                 </span>
-            </div>
-            <div>
+                </div>
+                <div>
+                    {/* not align center in this annotation https://zhuanlan.zhihu.com/p/28626505 
                 <span className="pointer" onClick={ () => { } }>
                     <Icon name='radioBoxBlank' />
                 </span>
-                <input />
+                <textarea />
+                <input/> */}
+                    { this.state.todoArr.map((item, index) => {
+                        const todoId = item.id
+                        const focusing = todoId === this.state.focusTodoId
+                        const textareaRef = focusing ? this.textareaRef : Object.create(null)
+                        return (
+                            <TodoItem
+                                key={ todoId }
+                                id={ todoId }
+                                index={ index }
+                                info={ item }
+                                changeFocusId={ this.changeFocusId }
+                                focusing={ focusing }
+                                textareaRef={ textareaRef }
+                                editTodo={ this.editTodo }
+                            />
+                        )
+                    }) }
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 export default Todo
---
+++ frontend/my-app/src/components/workspaces/todo/TodoItem.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Icon from '../../icons/Icon'
+
+export default class TodoItem extends Component {
+    static propTypes = {
+        index: PropTypes.number.isRequired,
+        info: PropTypes.object.isRequired,
+        changeFocusId: PropTypes.func.isRequired,
+        focusing: PropTypes.bool.isRequired,
+        textareaRef: PropTypes.object.isRequired,
+        editTodo: PropTypes.func.isRequired
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            content: props.info.content
+        }
+        this.textareaRef = React.createRef();
+    }
+
+    componentDidMount() {
+        const { focusing } = this.props
+        if (focusing) {
+            this.focus(this.textareaRef)
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.focusing === false && this.props.focusing === true) {
+            this.focus(this.textareaRef)
+        }
+    }
+
+    focus() {
+        this.textareaRef.current.focus()
+    }
+
+    render() {
+        const { index, info, changeFocusId, focusing, editTodo } = this.props
+        return (
+            <div data-id={ info.id } onClick={ () => { changeFocusId(info.id) } }>
+                <span className="pointer" onClick={ () => { } }>
+                    <Icon name='radioBoxBlank' />
+                </span>
+                <textarea
+                    rows="1"
+                    className={ focusing ? '' : 'hide' }
+                    ref={ this.textareaRef }
+                    value={ this.state.content }
+                    onChange={ (e) => { this.setState({ content: e.target.value }) } }
+                    onBlur={ () => { editTodo(info, index) } }
+                ></textarea>
+                <span className={ focusing ? 'hide' : '' }>{ this.state.content }</span>
+            </div>
+        )
+    }
+}
