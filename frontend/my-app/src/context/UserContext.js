d672ecc38d928b7b282ef8aa8a864ecb45283e7a
巫镇泉 <wuzhenquan1990@gmail.com>
2019-06-07 13:21:02 +0800

    update

--- frontend/my-app/src/context/UserContext.js
+++ frontend/my-app/src/context/UserContext.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
+// create context with default values
+// default values is only used when a component does not have a matching Provider above in the tree
 export default React.createContext({
     name: '',
-    authenticate: () => { }
+    authenticate: () => {}
 })
