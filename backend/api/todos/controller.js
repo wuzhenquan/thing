34a8a11df0442a0ed90362df81a626a3fb17978f
巫镇泉 <wuzhenquan1990@gmail.com>
2019-08-11 13:43:15 +0800

    **backend** bug fix

--- backend/api/todos/controller.js
+++ backend/api/todos/controller.js
@@ -1,7 +1,8 @@
 const todo = require('./model')
 
 exports.read = () => {
-    return todo.find()
+    // learn from https://stackoverflow.com/questions/5825520/in-mongoose-how-do-i-sort-by-date-node-js
+    return todo.find().sort({update_on: 'descending'})
 }
 
 exports.create = (data = {}) => {
