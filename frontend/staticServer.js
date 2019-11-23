4c71877ef6c8a84b1a45103ee1d3b5b91a7d9980
巫镇泉 <wuzhenquan1990@gmail.com>
2019-11-23 10:12:38 +0800

    Create staticServer.js

---
+++ frontend/staticServer.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const newPath = path.join(__dirname, '/React/build')
+
+app.use(express.static(newPath));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(newPath, 'index.html'));
+});
+app.listen(80);
