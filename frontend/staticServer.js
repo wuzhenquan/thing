640abcb47df2664709aa68cec5f603b15e39a3d3
巫镇泉 <wuzhenquan1990@gmail.com>
2020-02-01 20:17:03 +0800

    **backend** Serving Apps with Client-Side Routing

--- frontend/staticServer.js
+++ frontend/staticServer.js
@@ -4,7 +4,7 @@ const app = express();
 const newPath = path.join(__dirname, '/React/build')
 
 app.use(express.static(newPath));
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(newPath, 'index.html'));
 });
 app.listen(80);
