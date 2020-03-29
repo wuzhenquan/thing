34ccc3604a8404d6b16fbfab05a047677b22be1e
巫镇泉 <wuzhenquan1990@gmail.com>
2020-03-29 23:07:43 +0800

    **frontend** convert SignIn.jsx
    convert SignIn.jsx Class Component to Functional Component with React Hooks

--- frontend/React/src/components/sign/SignIn.jsx
+++ frontend/React/src/components/sign/SignIn.jsx
@@ -1,46 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import * as api from '../../api'
 import Icon from '../icons/Icon'
 import { withRouter } from 'react-router-dom'
-import PropTypes from 'prop-types'
 import { JSEncrypt } from 'jsencrypt'
 import WithCommonContext from '../../context/common/WithCommonContext'
 import UserContext from '../../context/user/UserContext'
 
-class SignIn extends Component {
-  static propTypes = {
-    history: PropTypes.object.isRequired
-  }
+function SignIn(props) {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      password: ''
-    }
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const {
       commonContext: { getPublicKey }
-    } = this.props
+    } = props
     getPublicKey()
-  }
+  })
 
-  async submit(authenticate, e) {
-    const {
-      commonContext: { publicKey }
-    } = this.props
+  const submit = async (authenticate, e) => {
     e && e.preventDefault() // stop the page trying to load the action url.
-    const { history } = this.props
-    if (!this.state.name) return console.error('请输入名字')
-    else if (!this.state.password) return console.error('请输入密码')
+    const {
+      commonContext: { publicKey },
+      history
+    } = props
+    if (!name) return console.error('请输入名字')
+    else if (!password) return console.error('请输入密码')
     let encryptor = new JSEncrypt() //实例化
     encryptor.setPublicKey(publicKey) //设置公钥
     api
       .signIn({
-        name: this.state.name,
-        password: encryptor.encrypt(this.state.password)
+        name,
+        password: encryptor.encrypt(password)
       })
       .then(user => authenticate(user))
       .then(() => {
@@ -48,70 +39,63 @@ class SignIn extends Component {
       })
   }
 
-  render() {
-    return (
-      <section className="hero is-fullheight-with-navbar level">
-        <UserContext.Consumer>
-          {({ authenticate }) => (
-            <form
-              onSubmit={e => {
-                this.submit(authenticate, e)
-              }}
-              className="level-item has-text-centered"
-            >
-              <div>
-                <div className="field">
-                  <div className="control has-icons-left has-icons-right">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="name"
-                      onChange={e => {
-                        this.setState({ name: e.target.value })
-                      }}
-                      required
-                    />
-                    <span className="icon is-small is-left">
-                      <Icon name="account" size={0.9} />
-                    </span>
-                    <span className="icon is-small is-right">
-                      <i className="fas fa-check"></i>
-                    </span>
-                  </div>
-                </div>
-                <div className="field">
-                  <div className="control has-icons-left">
-                    <input
-                      className="input"
-                      type="password"
-                      placeholder="Password"
-                      onChange={e => {
-                        this.setState({ password: e.target.value })
-                      }}
-                      required
-                    />
-                    <span className="icon is-small is-left">
-                      <Icon name="lock" size={0.9} />
-                    </span>
-                  </div>
-                </div>
-                <div className="field">
-                  <p className="control">
-                    <button className="button is-primary is-fullwidth">Sign in</button>
-                  </p>
-                </div>
-                <div>
-                  <p className="is-size-7">
-                    Don't have an account? <a href="/signup">Sign up.</a>
-                  </p>
+  return (
+    <section className="hero is-fullheight-with-navbar level">
+      <UserContext.Consumer>
+        {({ authenticate }) => (
+          <form onSubmit={e => submit(authenticate, e)} className="level-item has-text-centered">
+            <div>
+              <div className="field">
+                <div className="control has-icons-left has-icons-right">
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="name"
+                    onChange={e => setName(e.target.value)}
+                    required
+                  />
+                  <span className="icon is-small is-left">
+                    <Icon name="account" size={0.9} />
+                  </span>
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-check"></i>
+                  </span>
                 </div>
               </div>
-            </form>
-          )}
-        </UserContext.Consumer>
-      </section>
-    )
-  }
+              <div className="field">
+                <div className="control has-icons-left">
+                  <input
+                    className="input"
+                    type="password"
+                    placeholder="Password"
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                  />
+                  <span className="icon is-small is-left">
+                    <Icon name="lock" size={0.9} />
+                  </span>
+                </div>
+              </div>
+              <div className="field">
+                <p className="control">
+                  <button className="button is-primary is-fullwidth">Sign in</button>
+                </p>
+              </div>
+              <div>
+                <p className="is-size-7">
+                  Don't have an account? <a href="/signup">Sign up.</a>
+                </p>
+              </div>
+            </div>
+          </form>
+        )}
+      </UserContext.Consumer>
+    </section>
+  )
+}
+
+SignIn.propTypes = {
+  history: PropTypes.object.isRequired
 }
 
 export default withRouter(WithCommonContext(SignIn))
