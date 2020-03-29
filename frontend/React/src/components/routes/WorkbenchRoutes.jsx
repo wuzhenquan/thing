eda6cca7fa1f62a3997da826079c7ee4c77961ad
巫镇泉 <wuzhenquan1990@gmail.com>
2020-03-29 19:32:45 +0800

    **frontend** convert WorkBenchRoutes.jsx
    convert WorkBenchRoutes.jsx Class Component to Functional Component with React Hooks

--- frontend/React/src/components/routes/WorkbenchRoutes.jsx
+++ frontend/React/src/components/routes/WorkbenchRoutes.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Sidebar from '../sidebar/Sidebar'
 import { Route } from 'react-router-dom'
 import Todo from '../workspaces/todo/Todo'
 import Developing from '../workspaces/Developing'
 import './workbench.scss'
 
-export default class WorkbenchRoutes extends Component {
-  render() {
-    return (
-      <div className="columns is-fullheight">
-        <Sidebar />
-        <div className="column is-main-content">
-          <Route path="/workbench/todo" component={Todo} />
-          <Route path="/workbench/okr" component={Developing} />
-        </div>
+function WorkbenchRoutes() {
+  return (
+    <div className="columns is-fullheight">
+      <Sidebar />
+      <div className="column is-main-content">
+        <Route path="/workbench/todo" component={Todo} />
+        <Route path="/workbench/okr" component={Developing} />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default WorkbenchRoutes
