86860e46e058376911e60457f6f064b0a2f57523
巫镇泉 <wuzhenquan1990@gmail.com>
2020-04-04 15:53:53 +0800

    **frontend** convert UserDropDown.jsx
    convert UserDropDown.jsx Class Component to Functional Component with React Hooks

--- frontend/React/src/components/user/UserDropDown.jsx
+++ frontend/React/src/components/user/UserDropDown.jsx
@@ -1,34 +1,26 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Icon from '../icons/Icon'
 import BtnSignOut from '../sign/BtnSignOut'
 
-export default class UserDropDown extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isActive: false
-    }
-  }
-
-  render() {
-    return (
-      <div className={`dropdown is-right ${this.state.isActive ? 'is-active' : ''}`}>
-        <div
-          className="dropdown-trigger"
-          onClick={() => {
-            this.setState(state => ({ isActive: !state.isActive }))
-          }}
-        >
-          <Icon name="account" size={0.9} />
-        </div>
-        <div className="dropdown-menu" id="dropdown-menu4" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <BtnSignOut>sign out</BtnSignOut>
-            </div>
+export default function UserDropDown() {
+  const [isActive, setIsActive] = useState(false)
+  return (
+    <div className={`dropdown is-right ${isActive ? 'is-active' : ''}`}>
+      <div
+        className="dropdown-trigger"
+        onClick={() => {
+          setIsActive(!isActive)
+        }}
+      >
+        <Icon name="account" size={0.9} />
+      </div>
+      <div className="dropdown-menu" id="dropdown-menu4" role="menu">
+        <div className="dropdown-content">
+          <div className="dropdown-item">
+            <BtnSignOut>sign out</BtnSignOut>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
