40875d0de1c9bdb2d6cf3cea2c7698096d16d901
巫镇泉 <wuzhenquan1990@gmail.com>
2020-04-05 10:49:45 +0800

    **frontend** convert TodoItem.jsx
    convert TodoItem.jsx Class Component to Functional Component with React Hooks

--- frontend/React/src/components/workspaces/todo/TodoItem.jsx
+++ frontend/React/src/components/workspaces/todo/TodoItem.jsx
@@ -1,74 +1,49 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import Icon from '../../icons/Icon'
 
-export default class TodoItem extends Component {
-  static propTypes = {
-    index: PropTypes.number.isRequired,
-    info: PropTypes.object.isRequired,
-    changeFocusId: PropTypes.func.isRequired,
-    focusing: PropTypes.bool.isRequired,
-    editTodo: PropTypes.func.isRequired
-  }
+function TodoItem(props) {
+  const [content, setContent] = useState(props.info.content)
+  const { focusing, index, info, changeFocusId, editTodo, deleteTodo } = props
+  const textareaRef = useRef()
+  useEffect(() => {
+    if (focusing) focus(textareaRef)
+  }, [focusing])
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      content: props.info.content
-    }
-    this.textareaRef = React.createRef()
+  const focus = () => {
+    textareaRef.current.focus()
   }
-
-  componentDidMount() {
-    const { focusing } = this.props
-    if (focusing) {
-      this.focus(this.textareaRef)
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.focusing === false && this.props.focusing === true) {
-      this.focus(this.textareaRef)
-    }
-  }
-
-  focus() {
-    this.textareaRef.current.focus()
-  }
-
-  render() {
-    const { index, info, changeFocusId, editTodo, deleteTodo } = this.props
-    return (
-      <div
-        className="todo-item"
-        onClick={() => {
-          changeFocusId(info.id)
-        }}
-      >
-        <span className="pointer" onClick={() => {}}>
-          <Icon name="radioBoxBlank" />
-        </span>
-        <input
-          rows="1"
-          ref={this.textareaRef}
-          value={this.state.content}
-          onChange={e => {
-            this.setState({ content: e.target.value })
-          }}
-          onBlur={e => {
-            editTodo(info, index, e.target.value)
-          }}
-        />
-        {/* <span className={ focusing ? 'hide' : '' }>{ this.state.content }</span> */}
-        <span
-          className="pointer buttons-right"
-          onClick={() => {
-            deleteTodo(info.id, index)
-          }}
-        >
-          <Icon name="delete" />
-        </span>
-      </div>
-    )
-  }
+  return (
+    <div
+      className="todo-item"
+      onClick={() => {
+        changeFocusId(info.id)
+      }}
+    >
+      <span className="pointer" onClick={() => {}}>
+        <Icon name="radioBoxBlank" />
+      </span>
+      <input
+        rows="1"
+        ref={textareaRef}
+        value={content}
+        onChange={e => setContent(e.target.value)}
+        onBlur={e => editTodo(info, index, e.target.value)}
+      />
+      {/* <span className={ focusing ? 'hide' : '' }>{content}</span> */}
+      <span className="pointer buttons-right" onClick={() => deleteTodo(info.id, index)}>
+        <Icon name="delete" />
+      </span>
+    </div>
+  )
 }
+
+TodoItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  info: PropTypes.object.isRequired,
+  changeFocusId: PropTypes.func.isRequired,
+  focusing: PropTypes.bool.isRequired,
+  editTodo: PropTypes.func.isRequired
+}
+
+export default TodoItem
